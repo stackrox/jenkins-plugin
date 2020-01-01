a68e18602de576d306ec141a89f62650107566e3
Akshat Mishra <akshat@stackrox.com>
2020-01-01 14:18:22 -0800

    base speficiation indendation

--- functionaltest-jenkins-plugin/src/test/groovy/BaseSpecification.groovy
+++ functionaltest-jenkins-plugin/src/test/groovy/BaseSpecification.groovy
@@ -1,17 +1,10 @@
 import spock.lang.Specification
-import java.util.Map
-import java.util.HashMap
-
-
 
 class BaseSpecification extends Specification {
+
     RestApiClient restApiClient
     def setup() {
         restApiClient = new RestApiClient()
     }
     def cleanup() { }
-
-
-
-
 }
