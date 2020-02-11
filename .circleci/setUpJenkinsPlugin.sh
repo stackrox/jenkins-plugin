1510720da69fb33afadd37a1fab628c86e7ec5ef
Akshat Mishra <akshat@stackrox.com>
2020-02-11 12:36:40 -0800

    exit 1 and few other changes

--- .circleci/setUpJenkinsPlugin.sh
+++ .circleci/setUpJenkinsPlugin.sh
@@ -6,7 +6,6 @@ for i in $(seq 1 50); do
    export JENKINSPOD="$(kubectl get pods -n qa  -o=jsonpath='{range .items[*]}{.metadata.name}')"
    if [[ -n "${JENKINSPOD}" ]]; then
       SUCCESS=0
-      echo "Jenkins Pod is ready"
       echo "JENKINSPOD is running on ${JENKINSPOD}"
       break
    fi
@@ -17,23 +16,32 @@ if [[ "${SUCCESS}" == 1 ]]; then
        kubectl -n qa get svc
        kubectl -n qa get pods
        echo "Failed to deploy jenkins pod"
+       exit 1
 fi
 kubectl cp /home/circleci/jenkins-plugin/stackrox-container-image-scanner/target/stackrox-container-image-scanner.hpi qa/${JENKINSPOD}:/var/jenkins_home/plugins/.
 output="$(kubectl -n qa  exec -i ${JENKINSPOD} ls /var/jenkins_home/plugins/stackrox-container-image-scanner.hpi)"
 if [[ $? -eq 0 ]]; then
     echo "Jenkins plugin has been installed"
+  else
+    echo "Jenkins plugin failed to install"
+    exit 1
 fi
-export JENKINSVC=$(kubectl get svc -n qa jenkinsep -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
-echo Jenkins svc is running on "${JENKINSVC}"
+for i in $(seq 1 50); do
+  export JENKINSVC=$(kubectl get svc -n qa jenkinsep -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+   if [[ -n "${JENKINSVC}" ]]; then
+      echo "Jenkins svc is running on ${JENKINSVC}"
+      break
+   fi
+done
 echo restarting jenkins
 export JENKINS_URL="http://${JENKINSVC}:${JENKINSPORT}/"
 curl -XPOST "${JENKINS_URL}/restart"
 for i in $(seq 1 50); do
   output="$(curl -sk --connect-timeout 5 --max-time 10 "${JENKINS_URL}")"
   if [[ $? -eq 0 ]]; then
-        break
         echo "stackrox plugin installation on Jenkins is complete"
-    fi
+        break
+  fi
   sleep 5
 done
 echo "Jenkins installation is complete"
