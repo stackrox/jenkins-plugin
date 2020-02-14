7f7d9429bea5d31b1383f6e8433c94e9014537b0
Akshat Mishra <56654918+akshatrox@users.noreply.github.com>
2020-02-14 13:19:58 -0800

    ROX-3806 - Deploy Jenkins into GKE and configure StackRox plugin (#28)
    * pr for jenkins deployment
    
    * steps
    
    * fix path for jenkins plugin
    
    * move the setupplugin to its own shell script
    
    * setUpJenkinsPlugin in executable mode
    
    * set up jenkins plugin
    
    * exit 1 and few other changes
    
    * add some logic in the bash scripting to exit gracefully
    
    * check for get svc
    
    * missing dollar sign
    
    * cancel workflow/job
    
    * missing dollar sign
    
    * mirroring logic
    
    * reverse
    
    * indent
    
    * review comments
    
    * review comments
    
    * review comments
    
    * review comments  && updates
    
    * jenkins svc up and running
    
    * configuration error
    
    * remove central svc deploymet
    
    * config and set up jenkins
    
    * changing the namespace to jenkins
    
    * changing the namespace to jenkins
    
    * replace namespace
    
    * add sleep
    
    * debugging
    
    * debugging
    
    * recheck the variables
    
    * in the else loop
    
    * debugging
    
    * pod namespaces updated
    
    * set up jenkins plugin
    
    * fix formatting
    
    * Added a new shell script for circle yaml
    
    * change permissions
    
    * review comments
    
    * formatting
    
    * udpate version
    
    * putting full back on
    
    * inline some yaml fragments

---
+++ .circleci/setUpJenkinsPlugin.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -e
+JENKINS_DEPLOYED=false
+JENKINSPORT="8080"
+for i in $(seq 1 50); do
+   JENKINSPOD="$(kubectl -n jenkins get pods -o=jsonpath='{.items[*].metadata.name}')"
+   export JENKINSPOD
+   if [[ -n "${JENKINSPOD}" ]]; then
+      JENKINS_DEPLOYED=true
+      echo "JENKINSPOD is running on ${JENKINSPOD}"
+      break
+   fi
+   sleep 5
+done
+if [[ "${JENKINS_DEPLOYED}" = false  ]]; then
+       kubectl -n jenkins describe deploy
+       kubectl -n jenkins describe rs
+       kubectl -n jenkins get svc
+       kubectl -n jenkins get pods
+       exit 1
+fi
+kubectl cp /home/circleci/jenkins-plugin/stackrox-container-image-scanner/target/stackrox-container-image-scanner.hpi jenkins/"${JENKINSPOD}":/var/jenkins_home/plugins/.
+result=$?
+if [[ $result -eq 0 ]]; then
+    echo "Jenkins plugin has been installed"
+  else
+    echo "Jenkins plugin failed to install"
+  exit 1
+fi
+kubectl -n jenkins exec -i "${JENKINSPOD}" ls /var/jenkins_home/plugins/stackrox-container-image-scanner.hpi
+GETSVC=false
+for i in $(seq 1 50); do
+  echo "in ${i} iteration"
+  JENKINSVC=$(kubectl get svc -n jenkins jenkins -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+  export JENKINSVC
+  if [[ -n "${JENKINSVC}" ]]; then
+      echo "Jenkins svc is running on ${JENKINSVC}"
+      GETSVC=true
+      break
+  fi
+  sleep 5
+done
+if [[ "$GETSVC" = false ]]; then
+  echo "Jenkins svc failed to come up"
+  exit 1
+fi
+echo restarting jenkins
+export JENKINS_URL="http://${JENKINSVC}:${JENKINSPORT}/"
+curl -XPOST "${JENKINS_URL}/restart"
+SERVICEREADY=false
+for i in $(seq 1 50); do
+  curl -sk --connect-timeout 5 --max-time 10 "${JENKINS_URL}"
+  result=$?
+  if [[ $result -eq 0 ]]; then
+    SERVICEREADY=true
+    echo "stackrox plugin installation on Jenkins is complete"
+    break
+  fi
+  sleep 5
+done
+if [[ "$SERVICEREADY" = true ]]; then
+    echo "Jenkins installation is complete"
+    exit 0
+  else
+       exit 1
+fi
