885fd4b77cf79cbe3a99405a712fb03b6c192b3b
Tomasz Janiszewski <janiszt@gmail.com>
2021-10-11 17:35:44 +0200

    Add more tests

--- stackrox-container-image-scanner/src/test/java/com/stackrox/jenkins/plugins/services/ImageServiceTest.java
+++ stackrox-container-image-scanner/src/test/java/com/stackrox/jenkins/plugins/services/ImageServiceTest.java
@@ -1,12 +1,15 @@
 package com.stackrox.jenkins.plugins.services;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import com.google.common.collect.ImmutableList;
+import com.stackrox.jenkins.plugins.data.CVE;
 import org.apache.http.HttpStatus;
 import static org.apache.http.HttpStatus.SC_OK;
 import org.junit.jupiter.api.AfterEach;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 class ImageServiceTest {
 
@@ -35,26 +39,83 @@ class ImageServiceTest {
 
     @Test
     public void shouldThrowOn500() throws IOException {
-        wireMockServer.stubFor(post(urlEqualTo("/v1/images/scan"))
-                .withHeader("Authorization", equalTo("Bearer some token"))
-                .withRequestBody(equalToJson("{\"imageName\" : \"jenkins:lts\",\"force\" : true}")
-                ).willReturn(aResponse()
-                        .withStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR)
-                        .withBody("{\"error\": \"some error\"}")));
+        wireMockServer.stubFor(postImagesScan().willReturn(aResponse()
+                .withStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .withBodyFile("v1/images/scan/error.json")));
 
         imageService = new ImageService(wireMockServer.baseUrl(), "some token", HttpClientUtils.get(false, null));
-        Exception exception = assertThrows(IOException.class, () -> imageService.getImageScanResults("jenkins:lts"));
+        Exception exception = assertThrows(IOException.class, () -> imageService.getImageScanResults("nginx:latest"));
         String expected = "Failed image scan request. Status code: 500. Error: ResponseEntityProxy{[Chunked: true]}";
         assertEquals(expected, exception.getMessage());
     }
 
     @Test
     public void shouldThrowWhenNoDataFor200() throws IOException {
-        wireMockServer.stubFor(post(urlEqualTo("/v1/images/scan"))
-                .withHeader("Authorization", equalTo("Bearer some token"))
-                .withRequestBody(equalToJson("{\"imageName\" : \"jenkins:lts\",\"force\" : true}")
-                ).willReturn(aResponse().withStatus(SC_OK).withBody("{}")));
+        wireMockServer.stubFor(postImagesScan().willReturn(
+                aResponse().withStatus(SC_OK).withBody("{}")));
         imageService = new ImageService(wireMockServer.baseUrl(), "some token", HttpClientUtils.get(false, null));
-        assertThrows(NullPointerException.class, () -> imageService.getImageScanResults("jenkins:lts"));
+        assertThrows(NullPointerException.class, () -> imageService.getImageScanResults("nginx:latest"));
+    }
+
+    @Test
+    public void shouldThrowWhenNoDaasdfasftadFor200() throws IOException {
+        wireMockServer.stubFor(postImagesScan().willReturn(
+                aResponse().withStatus(SC_OK).withBodyFile("v1/images/scan/nginx.latest.json")));
+        imageService = new ImageService(wireMockServer.baseUrl(), "some token", HttpClientUtils.get(false, null));
+        List<CVE> actual = imageService.getImageScanResults("nginx:latest");
+        ImmutableList<CVE> expected = ImmutableList.of(
+                CVE.Builder.newInstance().withId("CVE-2007-6755")
+                        .withCvssScore(5.8F)
+                        .withScoreType("V2")
+                        .withPublishDate("2013-10-11T22:55:00Z")
+                        .inPackage("openssl")
+                        .inVersion("1.1.1d-0+deb10u7")
+                        .isFixable(false)
+                        .withLink("https://security-tracker.debian.org/tracker/CVE-2007-6755")
+                        .build(),
+                CVE.Builder.newInstance().withId("CVE-2010-0928")
+                        .withCvssScore(4.0F)
+                        .withScoreType("V2")
+                        .withPublishDate("2010-03-05T19:30:00Z")
+                        .inPackage("openssl")
+                        .inVersion("1.1.1d-0+deb10u7")
+                        .isFixable(false)
+                        .withLink("https://security-tracker.debian.org/tracker/CVE-2010-0928")
+                        .build(),
+                CVE.Builder.newInstance().withId("CVE-2019-9893")
+                        .withCvssScore(9.8F)
+                        .withScoreType("V3")
+                        .withPublishDate("2019-03-21T16:01:00Z")
+                        .inPackage("libseccomp")
+                        .inVersion("2.3.3-4")
+                        .isFixable(false)
+                        .withLink("https://security-tracker.debian.org/tracker/CVE-2019-9893")
+                        .build());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFailOnMissingData() throws IOException {
+        wireMockServer.stubFor(postImagesScan().willReturn(
+                aResponse().withStatus(SC_OK).withBodyFile("v1/images/scan/minimal.json")));
+        imageService = new ImageService(wireMockServer.baseUrl(), "some token", HttpClientUtils.get(false, null));
+        List<CVE> actual = imageService.getImageScanResults("nginx:latest");
+        ImmutableList<CVE> expected = ImmutableList.of(
+                CVE.Builder.newInstance().withId("CVE-MISSING-DATA")
+                        .withCvssScore(0)
+                        .withScoreType("-")
+                        .withPublishDate("-")
+                        .inPackage("-")
+                        .inVersion("-")
+                        .isFixable(false)
+                        .withLink("-")
+                        .build());
+        assertEquals(expected, actual);
+    }
+
+    private MappingBuilder postImagesScan() {
+        return post(urlEqualTo("/v1/images/scan"))
+                .withHeader("Authorization", equalTo("Bearer some token"))
+                .withRequestBody(equalToJson("{\"imageName\" : \"nginx:latest\",\"force\" : true}"));
     }
 }
