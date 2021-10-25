78f6f994b9caea1997970959023cfb20901e4c78
Tomasz Janiszewski <janiszt@gmail.com>
2021-10-25 13:17:36 +0200

    Update stackrox-container-image-scanner/src/main/java/com/stackrox/jenkins/plugins/services/ApiClientFactory.java
    Co-authored-by: msugakov <537715+msugakov@users.noreply.github.com>

--- stackrox-container-image-scanner/src/main/java/com/stackrox/jenkins/plugins/services/ApiClientFactory.java
+++ stackrox-container-image-scanner/src/main/java/com/stackrox/jenkins/plugins/services/ApiClientFactory.java
@@ -40,8 +40,8 @@ public class ApiClientFactory {
     private static final int MAXIMUM_CACHE_SIZE = 5; // arbitrary chosen as there are no data to support this decision
 
     private static class CacheKey {
-        final private String caCert;
-        final private StackRoxTlsValidationMode tlsValidationMode;
+        private final String caCert;
+        private final StackRoxTlsValidationMode tlsValidationMode;
 
         private CacheKey(@Nullable String caCert, StackRoxTlsValidationMode tlsValidationMode) {
             this.caCert = caCert;
