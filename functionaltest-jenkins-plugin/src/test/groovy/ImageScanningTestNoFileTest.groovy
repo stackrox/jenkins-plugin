628129dbb824887ad1ede588248f14a473fb2b1c
Tomasz Janiszewski <tomek@redhat.com>
2023-03-27 16:48:04 +0200

    fix style

--- functionaltest-jenkins-plugin/src/test/groovy/ImageScanningTestNoFileTest.groovy
+++ functionaltest-jenkins-plugin/src/test/groovy/ImageScanningTestNoFileTest.groovy
@@ -3,6 +3,7 @@ import static JenkinsClient.createJobConfigNoFile
 class ImageScanningTestNoFileTest extends ImageScanningTest {
     @Override
     String getJobConfig(String imageName, Boolean policyEvalCheck, Boolean failOnCriticalPluginError) {
-        return createJobConfigNoFile(imageName, CENTRAL_URI, token, policyEvalCheck, failOnCriticalPluginError)
+        String image = QUAY_REPO + imageName
+        return createJobConfigNoFile(image, CENTRAL_URI, token, policyEvalCheck, failOnCriticalPluginError)
     }
 }
