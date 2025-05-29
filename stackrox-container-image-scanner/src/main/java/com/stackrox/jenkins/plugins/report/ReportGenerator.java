e256c545b7ba2f98da704f6a51d4f84849a1370b
RTann <rtannenb@redhat.com>
2025-05-29 11:24:00 -0700

    ROX-29531: include advisory in output

--- stackrox-container-image-scanner/src/main/java/com/stackrox/jenkins/plugins/report/ReportGenerator.java
+++ stackrox-container-image-scanner/src/main/java/com/stackrox/jenkins/plugins/report/ReportGenerator.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class ReportGenerator {
 
-    private static final String[] CVES_HEADER = {"COMPONENT", "VERSION", "CVE", "SEVERITY", "FIXABLE", "CVSS SCORE", "SCORE TYPE", "LINK"};
+    private static final String[] CVES_HEADER = {"COMPONENT", "VERSION", "CVE", "SEVERITY", "FIXABLE", "CVSS SCORE", "SCORE TYPE", "LINK", "ADVISORY", "ADVISORY LINK"};
     private static final String[] VIOLATED_POLICIES_HEADER = {"POLICY", "SEVERITY", "DESCRIPTION", "VIOLATION", "REMEDIATION", "ENFORCED"};
     private static final String CVES_FILENAME = "cves.csv";
     private static final String POLICY_VIOLATIONS_FILENAME = "policyViolations.csv";
@@ -54,7 +54,9 @@ public class ReportGenerator {
                         cve.isFixable(),
                         cve.getCvssScore(),
                         cve.getScoreType(),
-                        cve.getLink()
+                        cve.getLink(),
+                        cve.getAdvisory(),
+                        cve.getAdvisoryLink()
                 ));
             }
         }
